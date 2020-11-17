9a6747cafdd1916a5e64e58a5f100f4507a11f1c
Aiden <aiden.marques94@gmail.com>
2020-11-17 18:56:36 -0500

    commit tree 3c20d20b6715f8e833da125d8f6b5518b13a84e0

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+
+def prime?(num)
+    if num < 2
+      false
+    elsif
+    (2..(num - 1)).each do |prime|
+      if (num % prime) == 0
+        return false
+      end
+    end
+    true
+  end
+  end
